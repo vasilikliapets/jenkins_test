444e5d000928718ebd062846270b828565f4cb48
Vasiliy Kliapets <v_kliapets@wargaming.net>
2022-01-28 08:45:41 +0200

    change driver

--- Dockerfile
+++ Dockerfile
@@ -7,3 +7,4 @@ RUN apt-get install software-properties-common -y
 # RUN add-apt-repository ppa:deadsnakes/ppa -y
 RUN apt-get update -y
 RUN apt-get install python3
+RUN apt-get install python3-pip -y
--- conftest.py
+++ conftest.py
@@ -4,12 +4,12 @@ from selenium import webdriver
 
 @pytest.fixture(scope='session')
 def browser():
-    # chrome_options = webdriver.ChromeOptions()
-    # chrome_options.add_argument('--no-sandbox')
-    # chrome_options.add_argument('--headless')
-    # chrome_options.add_argument('--disable-gpu')
-    # driver = webdriver.Chrome(chrome_options=chrome_options)
-    driver = webdriver.Chrome()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+    # driver = webdriver.Chrome()
     driver.maximize_window()
     driver.implicitly_wait(5)
     yield driver
